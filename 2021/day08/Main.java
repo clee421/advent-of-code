1687fef69145e262e241dbc4092abe178bf91cc0
Calvin Lee <clee421@gmail.com>
2021-12-08 21:53:43 -0500

    aoc: 2021 d8 part2

--- 2021/day08/Main.java
+++ 2021/day08/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     var data = readFile("data-input");
-    var result = solve(data);
+    var result = solve2(data);
 
     System.out.println("result: " + result);
   }
@@ -30,6 +30,25 @@ public class Main {
     }
 
     return count;
+  }
+
+  private static long solve2(Vector<Entry> entries) {
+    int sum = 0;
+    for (int i = 0; i < entries.size(); i++) {
+      var entry = entries.elementAt(i);
+      // System.out.println("on row " + (i + 1));
+      var row = "";
+      for (int j = 0; j < entry.digitalOutputs.size(); j++) {
+        var seg = entry.digitalOutputs.elementAt(j);
+        // System.out.println(seg + " has value " + entry.getValue(seg));
+        row += entry.getValue(seg);
+      }
+
+      System.out.println(row);
+      sum += Integer.parseInt(row);
+    }
+
+    return sum;
   }
 
   private static Vector<Entry> readFile(String filename) {
@@ -58,12 +77,15 @@ class Entry {
   Vector<String> digitalOutputs;
 
   private HashMap<String, Integer> mapping;
+  private String[] reverseMap;
 
   public Entry(String line) {
-    // System.out.println(line);
+    // System.out.println("\n" + line);
     uniqSignalPatterns = new Vector<String>();
     digitalOutputs = new Vector<String>();
     mapping = new HashMap<String, Integer>();
+    reverseMap = new String[10];
+    Arrays.fill(reverseMap, "");
 
     String[] segmentParts = line.split("\\|");
     // System.out.println("parts" + segmentParts[0] + " ? " + segmentParts[1]);
@@ -95,26 +117,143 @@ class Entry {
   }
 
   private void calculateMapping() {
+    // find 1, 4, 7, 8
     for (int i = 0; i < uniqSignalPatterns.size(); i++) {
+      // System.out.println("\ncalc mapping on index " + i);
       var signal = uniqSignalPatterns.elementAt(i);
       // System.out.println("working on " + signal + " element " + i);
+
       if (signal.length() == 7) {
         // have all letters is an 8
+        // System.out.println("found " + signal + " to be 8");
         mapping.put(signal, 8);
+        reverseMap[8] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+        i -= 1;
       } else if (signal.length() == 4) {
+        // System.out.println("found " + signal + " to be 4");
         mapping.put(signal, 4);
+        reverseMap[4] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+        i -= 1;
       } else if (signal.length() == 3) {
+        // System.out.println("found " + signal + " to be 7");
         mapping.put(signal, 7);
+        reverseMap[7] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+        i -= 1;
       } else if (signal.length() == 2) {
+        // System.out.println("found " + signal + " to be 1");
         mapping.put(signal, 1);
+        reverseMap[1] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+        i -= 1;
       }
     }
+
+    // find 9, which overlaps with 4 & 7
+    for (int i = 0; i < uniqSignalPatterns.size(); i++) {
+      var signal = uniqSignalPatterns.elementAt(i);
+      if (
+        signal.length() == 6 &&
+        similarityScore(signal, reverseMap[4]) == 4 &&
+        similarityScore(signal, reverseMap[7]) == 3
+      ) {
+        // System.out.println("found " + signal + " to be 9");
+        mapping.put(signal, 9);
+        reverseMap[9] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+      }
+    }
+
+    // find 0, which overlaps with 7
+    for (int i = 0; i < uniqSignalPatterns.size(); i++) {
+      var signal = uniqSignalPatterns.elementAt(i);
+      if (
+        signal.length() == 6 &&
+        similarityScore(signal, reverseMap[7]) == 3
+      ) {
+        // System.out.println("found " + signal + " to be 0");
+        mapping.put(signal, 0);
+        reverseMap[0] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+      }
+    }
+
+    // find 6, which at this point is the only one left with 6 length
+    for (int i = 0; i < uniqSignalPatterns.size(); i++) {
+      var signal = uniqSignalPatterns.elementAt(i);
+      if (signal.length() == 6) {
+        // System.out.println("found " + signal + " to be 6");
+        mapping.put(signal, 6);
+        reverseMap[6] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+      }
+    }
+
+    // find 3, which overlaps with 9 & 7
+    for (int i = 0; i < uniqSignalPatterns.size(); i++) {
+      var signal = uniqSignalPatterns.elementAt(i);
+      if (
+        signal.length() == 5 &&
+        similarityScore(signal, reverseMap[7]) == 3 &&
+        similarityScore(signal, reverseMap[9]) == 5
+      ) {
+        // System.out.println("found " + signal + " to be 3");
+        mapping.put(signal, 3);
+        reverseMap[3] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+      }
+    }
+
+    // find 5, which overlaps with 9
+    for (int i = 0; i < uniqSignalPatterns.size(); i++) {
+      var signal = uniqSignalPatterns.elementAt(i);
+      if (signal.length() == 5 && similarityScore(signal, reverseMap[9]) == 5) {
+        // System.out.println("found " + signal + " to be 5");
+        mapping.put(signal, 5);
+        reverseMap[5] = signal;
+        uniqSignalPatterns.removeElementAt(i);
+      }
+    }
+
+    if (uniqSignalPatterns.size() != 1) {
+      System.out.println(uniqSignalPatterns);
+      System.err.println("wtf something broke");
+      System.exit(1);
+    }
+
+    // 2 is left
+    var signal = uniqSignalPatterns.elementAt(0);
+    // System.out.println("found " + signal + " to be 2");
+    mapping.put(signal, 2);
+    reverseMap[2] = signal;
+    uniqSignalPatterns.removeElementAt(0);
   }
 
   private String sort(String str) {
     char tempArray[] = str.toCharArray();
     Arrays.sort(tempArray);
     return new String(tempArray);
+  }
+
+  private int similarityScore(String left, String right) {
+    int score = 0;
+    var map = new HashSet<Character>();
+
+    var leftArray = left.toCharArray();
+    for (int i = 0; i < leftArray.length; i++) {
+      map.add(leftArray[i]);
+    }
+
+    var rightArray = right.toCharArray();
+    for (int i = 0 ; i < rightArray.length; i++) {
+      if (map.contains(rightArray[i])) {
+        score++;
+      }
+    }
+
+    return score;
   }
 }
 
