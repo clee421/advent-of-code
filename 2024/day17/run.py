951dee4b9816b4f844fcd9bd6c31775850a9baf3
Calvin Lee <clee421@gmail.com>
2025-02-04 21:36:16 -0500

    aoc: 2024 d17 part2

--- 2024/day17/run.py
+++ 2024/day17/run.py
@@ -1,4 +1,5 @@
 import re
+import time
 from typing import Dict, List, Tuple
 
 def parse_inputs(filepath: str) -> Tuple[Dict[str, int], List[int]]:
@@ -88,6 +89,67 @@ def execute_program(registers: Dict[str, int], program:  List[int]) -> List[int]
 
     return result
 
+def find_program_brute_force(registers: Dict[str, int], target_program:  List[int]) -> int:
+    start = time.time()
+    elapsed_time = start + 5
+
+    # MATCH = 12
+    # START = 220_141_272_079
+    # END = 3_000_000_000_000
+    # JUMP = 262144
+
+    MATCH = 12
+    START = 220_141_272_079
+    END = 3_000_000_000_000
+    JUMP = 262144
+
+    prev = 0
+    for i in range(END, START, -JUMP):
+        if time.time() > elapsed_time:
+            now = time.time()
+            elapsed_time = now + 10
+            print(f"Current value: {i}. Total elapsed time {now - start}")
+
+        cp = registers.copy()
+        cp["A"] = i
+        o = execute_program(cp, target_program)
+        if o == target_program:
+            return i
+
+        if o[:MATCH] == target_program[:MATCH]:
+            print(f"i {i} matched {MATCH} elements. i - {prev} = {i - prev}")
+            prev = i
+
+    return -1
+
+def test_octals(program):
+    registers = {"A": 0, "B": 0, "C": 0}
+
+    def run_oct_test(n):
+        cp = registers.copy()
+        cp["A"] = n
+        o = execute_program(cp, program)
+
+        # print(f"A: {n}, oct(A): {oct(n)}, output: {o} | {len(o)}")
+        if o == [2,4,1,2,7,5,0,3,4,7,1,7,5,5,3,0]:
+            print(f"A: {n}, oct(A): {oct(n)}, output: {o} | {len(o)}")
+
+    # for i in range(1, 11):
+    #     run_oct_test(i)
+
+    # for i in range(101, 111):
+    #     run_oct_test(i)
+
+    # for i in range(1001, 1011):
+    #     run_oct_test(i)
+
+    # for i in range(10001, 10011):
+    #     run_oct_test(i)
+
+    # for i in range(190_385_000_000_000, 190_386_000_000_000, 100_000_000):
+    for i in range(190384113100000, 190384113800000, 1):
+        run_oct_test(i)
+
 def part_01(args: List[str], options: Dict[str, any]):
     print("Running part 01", args, options)
     inputs = parse_inputs(options.get("filepath", args[0]))
@@ -99,5 +161,9 @@ def part_01(args: List[str], options: Dict[str, any]):
 def part_02(args: List[str], options: Dict[str, any]):
     print("Running part 02", args, options)
     inputs = parse_inputs(options.get("filepath", args[0]))
+    registers, program = inputs[0], inputs[1]
 
-    print("part 02")
+    test_octals(program)
+
+    # a_value = find_program_brute_force(registers.copy(), program)
+    # print("Register A", a_value)
